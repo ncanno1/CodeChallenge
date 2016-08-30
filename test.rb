a88b4617f53a45ff999020c819c2bba5767d59ff
ncanno1 <ncanno1@students.towson.edu>
2016-08-30 19:00:16 -0400

    Add potential tests

--- test.rb
+++ test.rb
@@ -10,6 +10,16 @@ class TestMain < Test::Unit::TestCase
 
   #signature should be properly encoded to UTF-8
   def test_signature_encoding
-    
+
+  end
+
+  #hash should be generated in the proper format
+  def test_hash_format
+
+  end
+
+  #JSON should be generated in the proper format
+  def test_json_format
+
   end
 end
