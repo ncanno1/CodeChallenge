6c98fa56cb1e52bc307c27565967b5442ccea0eb
ncanno1 <ncanno1@students.towson.edu>
2016-08-30 15:42:54 -0400

    Gets the signature

--- Signed_Identifier.rb
+++ Signed_Identifier.rb
@@ -7,4 +7,11 @@ message = ARGV.first
 #gets the public key from the file generated using OpenSSL
 pubkey = OpenSSL::PKey::RSA.new File.read 'public.pem'
 
+#creates digest, private key, then gets signature
+digest = OpenSSL::Digest::SHA256.new
+private_key = OpenSSL::PKey::RSA.new File.read 'private.pem'
+signature = private_key.sign(digest, message)
+
 puts message
+puts pubkey
+puts signature
