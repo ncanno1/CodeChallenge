89eded5f708da981f173510d3eccb5c4f902bc46
ncanno1 <ncanno1@students.towson.edu>
2016-08-30 14:53:52 -0400

    Gets the public key

--- Signed_Identifier.rb
+++ Signed_Identifier.rb
@@ -1,3 +1,10 @@
+#used to get the public key
+require 'OpenSSL'
+
+#gets the message as the first argument given when the file is run
 message = ARGV.first
+
+#gets the public key from the file generated using OpenSSL
+pubkey = OpenSSL::PKey::RSA.new File.read 'public.pem'
 
 puts message
